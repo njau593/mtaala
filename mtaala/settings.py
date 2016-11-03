64a642bd3ab8b0b01a7114326947305439eb5168
Godfrey Njau <njau593@users.noreply.github.com>
2016-11-03 08:57:18 +0300

    Update settings.py

--- mtaala/settings.py
+++ mtaala/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kx9299#b=)94uc-a^6*s0udc75$fv%gj9a!4yu4j$nzds-vl_6'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
